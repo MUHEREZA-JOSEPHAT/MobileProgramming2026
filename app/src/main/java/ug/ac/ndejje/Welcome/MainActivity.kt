84a99feebe5fa8208f99fd5dba4b444f483116ca
MUHEREZA-JOSEPHAT <muherezajosephat2@gmail.com>
2026-03-03 22:21:59 +0300

    added image

--- app/src/main/java/ug/ac/ndejje/Welcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/Welcome/MainActivity.kt
@@ -1,21 +1,37 @@
 package ug.ac.ndejje.Welcome
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ug.ac.ndejje.Welcome.ui.theme.NdejjeWelcomeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,8 +51,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun StudentInfo(){
+    Box(
+        modifier = Modifier
+            .height(30.dp)
+            .fillMaxWidth()
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(
+
+                    topStart = 2.dp,
+                    topEnd = 2.dp,
+
+                )
+            )
+    )
     Column(verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Image(painter = painterResource(id = R.drawable.mellisa9),
+                modifier = Modifier
+                    .size(70.dp)
+                    .border(2.dp, Color.Gray, RoundedCornerShape(80.dp))
+                    .clip(RoundedCornerShape(60.dp)),
+                contentScale = ContentScale.Crop,
+                contentDescription = "my image")
+
         Text(
             text = "Mellisa Nandyowa",
             style = MaterialTheme.typography.headlineSmall,
@@ -61,6 +100,8 @@ fun StudentInfo(){
 
                 )
             }
+        Image(painter = painterResource(id = R.drawable.barcode),
+            contentDescription = "The barcode")
 
     }
 }
