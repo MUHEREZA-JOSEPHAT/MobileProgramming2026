bbfde085c11f5c98128afad86726e8bdcce7ec63
MUHEREZA-JOSEPHAT <muherezajosephat2@gmail.com>
2026-03-03 16:48:54 +0300

    name,courseunit and regno

--- app/src/main/java/ug/ac/ndejje/Welcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/Welcome/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import ug.ac.ndejje.Welcome.ui.theme.NdejjeWelcomeAppTheme
 
@@ -19,29 +25,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NdejjeWelcomeAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    StudentInfo()
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StudentInfo(){
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "Mellisa Nandyowa",
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Bachelor Of Information Technology",
+                    style = MaterialTheme.typography.labelMedium,
+            color = MaterialTheme.colorScheme.tertiary
+        )
+
+            Row() {
+                Text(text = "REG NO.:",
+                    fontWeight = FontWeight.ExtraBold,
+                    style = MaterialTheme.typography.labelLarge
+
+                )
+                Text(text = " 24/2/314/D/683",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontFamily = FontFamily.Monospace
+
+                )
+            }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NdejjeWelcomeAppTheme {
-        Greeting("Android")
+fun NdejjePreview(){
+    NdejjeWelcomeAppTheme{
+        StudentInfo()
     }
 }
