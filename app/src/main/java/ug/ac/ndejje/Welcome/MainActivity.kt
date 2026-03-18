7e72196c0e76c8012923a948fc98a9e683931450
MUHEREZA_JOSEPHAT <muherezajosephat2@gmail.com>
2026-03-18 19:24:03 +0300

    Added search and activated button

--- app/src/main/java/ug/ac/ndejje/Welcome/MainActivity.kt
+++ app/src/main/java/ug/ac/ndejje/Welcome/MainActivity.kt
@@ -1,42 +1,47 @@
 package ug.ac.ndejje.Welcome
 
+import android.R
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-
 import androidx.compose.foundation.layout.fillMaxWidth
-
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
-
 import androidx.compose.material3.MaterialTheme
-
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ug.ac.ndejje.Welcome.ui.theme.NdejjeWelcomeAppTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,7 +56,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun StudentInfo(student: Student){
+fun StudentInfo(student: Student,verifiedStatus: Boolean?){
    Column(horizontalAlignment = Alignment.CenterHorizontally
        ) {
        Image(
@@ -71,10 +76,18 @@ fun StudentInfo(student: Student){
 
        Text(
            text = student.regNumber,
-           color = Color.Gray
+           color = Color.Black,
+           fontWeight = FontWeight.Bold
+
+
+
        )
-       if (student.isVerified){
-           Text("Verified Student",color = Color(0xFF4CAF50))
+
+
+       if (verifiedStatus == true) {
+           Text("Verified Student", color = Color(0xFF4CAF50))
+       } else if (verifiedStatus == false) {
+           Text("Student not verified", color = Color.Red)
        }
 
    }
@@ -82,14 +95,14 @@ fun StudentInfo(student: Student){
 
 @Composable
 fun StudentIdCard(student: Student){
-
+    var showStatus by remember { mutableStateOf<Boolean?>(null) }
 
     ElevatedCard(modifier = Modifier
         .fillMaxWidth()
         .padding(16.dp),
         shape = RoundedCornerShape(size = 16.dp),
         colors = CardDefaults.elevatedCardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.3f)
+            containerColor = MaterialTheme.colorScheme.primary
         )
 
     ) {
@@ -97,14 +110,13 @@ fun StudentIdCard(student: Student){
             modifier = Modifier
                 .padding(16.dp)
                 .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-        StudentInfo(student)
+        StudentInfo(student,showStatus)
             Button(onClick = {
-                student.isVerified
-
+                showStatus = if (showStatus == null) student.isVerified else null
             }) {
-                Text("view profile")
+                Text(if (showStatus == null) "View Profile" else "Hide Profile")
             }
         }
     }
@@ -112,16 +124,49 @@ fun StudentIdCard(student: Student){
 
 @Composable
 fun StudentDirectory(){
-    val students = StudentProvider.studentList
-    LazyColumn(modifier = Modifier
-        .fillMaxWidth(),
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        items(students){student ->
-            StudentIdCard( student = student)
+    var querySearch by remember { mutableStateOf("") }
+    val allStudents = StudentProvider.studentList
+    val filteredStudent by remember (querySearch){
+        derivedStateOf {
+            if(querySearch.isBlank()){
+                allStudents
+            }
+            else {
+                allStudents.filter{
+                    it.name.contains(querySearch, ignoreCase = true)
+                }
+            }
+
         }
     }
+    LazyColumn(modifier = Modifier
+        .fillMaxWidth(),
+
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        item() {
+            TextField(
+                value = querySearch,
+                onValueChange = { querySearch = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                placeholder = { Text("Search Student") },
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                shape = RoundedCornerShape(12.dp),
+
+            )
+
+        }
+
+        items(filteredStudent){student ->
+            StudentIdCard(student = student)
+        }
+    }
+
 }
+
+
 
 @Preview(showBackground = true,
     showSystemUi = true)
